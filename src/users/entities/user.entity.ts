d89ce57e2b3125f2acf8ced8d7d3a14825b53d76
Rishabh Pandey <rishabhpandey8092@gmail.com>
2025-08-20 23:09:27 +0530

    feat(User): Added user base emtity

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, Column, PrimaryColumn, PrimaryGeneratedColumn } from 'typeorm'
+
+@Entity()
+export class Users {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    username: string;
+}
